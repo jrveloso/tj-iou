0514b4ab227a4b282d2504970d071eae9e9cde24
Jordan Veloso <jrveloso96@gmail.com>
2024-11-03 21:36:28 -0600

    Changed mobile nav drawer into a burger icon.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,8 +6,21 @@ const Navbar = () => {
       <div className="drawer lg:hidden">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-start justify-center">
-          <label htmlFor="my-drawer-2" className="btn text-xl drawer-button">
-            prof
+          <label htmlFor="my-drawer-2" className="btn text-xl drawer-button bg-base-100 border-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
           </label>
         </div>
         <div className="drawer-side">
