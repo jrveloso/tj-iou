2da8c982384423e2f8fd4e27773e238d89bb8df9
Jordan Veloso <jrveloso96@gmail.com>
2024-11-05 20:04:54 -0600

    Changed employee ID form to a modal popup.

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -2,35 +2,22 @@ import React, { useState } from "react";
 import ListItem from "./ListItem";
 
 const List = ({ ious, setIOUs, userID, setAlert }) => {
-  const checkedIOUs = {}
-  for(const iou of ious) {
-    checkedIOUs[iou._id] = false
+  const checkedIOUs = {};
+  for (const iou of ious) {
+    checkedIOUs[iou._id] = false;
   }
-  // console.log(checkedIOUs)
+
   const [checkedItems, setCheckedItems] = useState(checkedIOUs);
-  const [popup, setPopup] = useState(false);
+  const [itemPopup, setItemPopup] = useState(false);
   const [employeeID, setEmployeeID] = useState();
 
   const handleCheck = (e) => {
-    // if (paid.includes(id)) {
-    //   setPaid((prev) => prev.filter((iou) => iou !== id));
-    // } else {
-    //   const newPaid = ious.find((iou) => iou._id === id);
-    //   setPaid((prev) => [...prev, newPaid._id]);
-    // }
-    // console.log(paid);
-    const { id, checked } = e.target
-    console.log(e)
+    const { id, checked } = e.target;
+    console.log(e);
     setCheckedItems((prevState) => ({
       ...prevState,
       [id]: checked,
-    }))
-  };
-
-  const handlePay = (e) => {
-    e.preventDefault()
-    // const items = ious.filter((iou) => paid.includes(iou._id));
-    setPopup(true);
+    }));
   };
 
   const handleSubmit = async (e) => {
@@ -39,9 +26,8 @@ const List = ({ ious, setIOUs, userID, setAlert }) => {
       alert("Must have a coworker check you out");
       return;
     }
-    // console.log(checkedIOUs)
-    const paid = Object.keys(checkedItems)
-    // console.log(paid)
+
+    const paid = Object.keys(checkedItems);
 
     const response = await fetch("http://localhost:5003/api/ious/pay", {
       method: "PUT",
@@ -51,19 +37,57 @@ const List = ({ ious, setIOUs, userID, setAlert }) => {
       body: JSON.stringify({ ids: paid }),
     });
     if (response.ok) {
-      // alert(`${paid.length} IOUs paid successfully.`);
-      setAlert(true)
+      setAlert(true);
       setIOUs((prevList) => prevList.filter((iou) => !paid.includes(iou._id)));
     }
 
-    setPopup(false);
+    setItemPopup(false);
   };
 
   return (
     <div>
-      {popup && (
-        <div>
-          <form onSubmit={handleSubmit}>
+      <form className="py-2 flex flex-col">
+        {ious.map((iou, i, a) => {
+          if (i === 0 || iou.date !== a[i - 1].date) {
+            return (
+              <>
+                <h3 className="text-xl font-medium">{iou.date}</h3>
+                <ListItem
+                  id={iou._id}
+                  name={iou.name}
+                  sku={iou.sku}
+                  handleCheck={handleCheck}
+                  checkedItems={checkedItems}
+                />
+              </>
+            );
+          } else {
+            return (
+              <ListItem
+                id={iou._id}
+                name={iou.name}
+                sku={iou.sku}
+                handleCheck={handleCheck}
+                checkedItems={checkedItems}
+              />
+            );
+          }
+        })}
+      </form>
+      <>
+      <button
+        className="btn btn-primary w-full"
+        onClick={() => document.getElementById("my_modal_4").showModal()}
+      >
+        Pay
+      </button>
+      <dialog id="my_modal_4" className="modal">
+        <div className="modal-box">
+          <form method="dialog" onSubmit={handleSubmit}>
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+            <h3>Enter Employee ID</h3>
             <input
               type="number"
               value={employeeID}
@@ -76,26 +100,8 @@ const List = ({ ious, setIOUs, userID, setAlert }) => {
             </button>
           </form>
         </div>
-      )}
-      <form className="py-2 flex flex-col">
-        {ious.map((iou, i, a) => {
-          if (i === 0 || iou.date !== a[i - 1].date) {
-            return (
-              <>
-                <h3 className="text-xl font-medium">{iou.date}</h3>
-                <ListItem id={iou._id} name={iou.name} sku={iou.sku} handleCheck={handleCheck} checkedItems={checkedItems}/>
-              </>
-            );
-          } else {
-            return (
-              <ListItem id={iou._id} name={iou.name} sku={iou.sku} handleCheck={handleCheck} checkedItems={checkedItems}/>
-            );
-          }
-        })}
-      <button onClick={handlePay} className="btn btn-primary">
-        Pay
-      </button>
-      </form>
+      </dialog>
+      </>
     </div>
   );
 };
