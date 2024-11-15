1b538a0612f335d1e5f8303c910d7ab3b23cddd1
Jordan Veloso <jrveloso96@gmail.com>
2024-11-15 15:11:22 -0600

    Configured app to host on fly.io.

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,12 @@ const authRoutes = require('./routes/authRoutes')
 
 require("dotenv").config({path: './config/.env'});
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 connectDB()
 
 const app = express();
